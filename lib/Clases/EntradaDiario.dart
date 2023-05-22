cdd28e552a334f0f0be96ee2c16838d2826b96c6
unknown <valeria.espinal@upb.edu.co>
2023-05-22 16:15:15 -0500

    vista de diario y clase entradadiario

---
+++ lib/Clases/EntradaDiario.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class EntradaDiario extends StatelessWidget {
+  final String emocion;
+  final String texto;
+  final List<String> tags;
+  final String fecha;
+
+  const EntradaDiario({
+    required this.emocion,
+    required this.texto,
+    required this.tags,
+    required this.fecha,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/Views/DiarioView.dart
@@ -0,0 +1,322 @@
+import 'package:flutter/material.dart';
+
+class DiarioView extends StatelessWidget {
+  const DiarioView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 80,
+              color: Color(0xFF736CED),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      // Acción al presionar el icono de flecha hacia la izquierda
+                    },
+                    icon: Icon(Icons.arrow_back),
+                    color: Colors.white,
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        'Diario',
+                        style: TextStyle(
+                          fontFamily: 'Boba Milky',
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Acción al presionar el primer container
+              },
+              child: Container(
+                height: 200,
+                color: Color(0xFFFEF9FF),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/emociones/sonrisa.png',
+                      width: 150,
+                      height: 150,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Alegre',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Color(0xFF736CED),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            TagWidget(text: 'mascota'),
+                            TagWidget(text: 'familia'),
+                            TagWidget(text: 'pareja'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Fecha y hora',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Acción al presionar el segundo container
+              },
+              child: Container(
+                height: 200,
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/emociones/sonrisa (1).png',
+                      width: 150,
+                      height: 150,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Optimista',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Color(0xFF736CED),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            TagWidget(text: 'descanso'),
+                            TagWidget(text: 'amigos'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Fecha y hora',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Acción al presionar el tercer container
+              },
+              child: Container(
+                height: 200,
+                color: Color(0xFFFEF9FF),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/emociones/triste.png',
+                      width: 150,
+                      height: 150,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Herido',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Color(0xFF736CED),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            TagWidget(text: 'trabajo'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Fecha y hora',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Acción al presionar el cuarto container
+              },
+              child: Container(
+                height: 200,
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/emociones/pensado.png',
+                      width: 150,
+                      height: 150,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Deprimido',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Color(0xFF736CED),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            TagWidget(text: 'familia'),
+                            TagWidget(text: 'amigos'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Fecha y hora',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Acción al presionar el quinto container
+              },
+              child: Container(
+                height: 200,
+                color: Color(0xFFFEF9FF),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/emociones/frio.png',
+                      width: 150,
+                      height: 150,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Orgulloso',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Color(0xFF736CED),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            TagWidget(text: 'logro'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Fecha y hora',
+                          style: TextStyle(
+                            fontFamily: 'Sniglet',
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Acción al presionar el texto "sobre la aplicación"
+              },
+              child: Text(
+                'Sobre la aplicación',
+                style: TextStyle(
+                  fontFamily: 'Boba Milky',
+                  color: Color(0xFF736CED),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TagWidget extends StatelessWidget {
+  final String text;
+
+  const TagWidget({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 10),
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+        color: Color(0xFFD4C1EC),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontFamily: 'Sniglet',
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
