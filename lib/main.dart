eb0404be97c22eef0ad9acfb3a78aed598f1371c
unknown <valeria.espinal@upb.edu.co>
2023-05-23 19:46:28 -0500

    vista principal, cambios al login,2

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:emook/Views/login_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
